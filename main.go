980aeb5c7aea8212b93c6d07d662b9f99e3a587a
Nader Rahhal <107228500+Nader-Rahhal@users.noreply.github.com>
2024-09-22 18:25:40 -0500

    templ

--- main.go
+++ main.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+    "net/http"
+    "github.com/labstack/echo/v4"
+)
+
+type Task struct {
+    ID        string `json:"id"`
+    Title     string `json:"title"`
+    Completed bool   `json:"completed"`
+}
+
+var tasks = []Task{
+    {ID: "1", Title: "clean sink", Completed: false},
+    {ID: "2", Title: "clean room", Completed: false},
+    {ID: "3", Title: "clean floor", Completed: false},
+}
+
+func getTasks(c echo.Context) error {
+    return c.JSON(http.StatusOK, tasks)
+}
+
+func main() {
+    e := echo.New()
+    e.GET("/tasks", getTasks)
+    e.Logger.Fatal(e.Start(":8080"))
+}
